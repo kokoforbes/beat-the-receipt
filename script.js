7bfeefa706e29a8f509cd47d3c242eeeeb31a5a9
Francis Maduakor <francis.maduakor@tivra.ai>
2021-06-09 11:25:57 +0800

    Changes made

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 function rangeSlide(value) {
   document.getElementById("rangeValue").innerHTML = value;
 
-  var a = value * 130;
+  var a = value * 130/1000;
   var b = value * 13;
   var c = value * 0.26;
   var d = value * 0.05;
 
-  document.getElementById("carbon").innerHTML = a.toFixed(0) + "g";
+  document.getElementById("carbon").innerHTML = a.toFixed(2) + "kg";
   document.getElementById("bags").innerHTML = b.toFixed(0);
   document.getElementById("beer").innerHTML = c.toFixed(0);
   document.getElementById("burger").innerHTML = d.toFixed(0);
